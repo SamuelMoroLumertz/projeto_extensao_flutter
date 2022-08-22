2ec16e250ee2da6a4791a5c900ecd4a615181902
Symon <samuelmoro_l@hotmail.com>
2022-08-22 09:25:57 -0300

    Primeira Atualização

---
+++ flutter_application/lib/pages/meetings_page.dart
@@ -0,0 +1,71 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart' as material;
+
+import '../models/responsable_manager.dart';
+import '../utils/window_buttons.dart';
+
+class MeetingsPage extends StatelessWidget {
+  const MeetingsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return material.Scaffold(
+      body: WindowBorder(
+        width: 1,
+        color: const Color.fromARGB(255, 75, 62, 99),
+        child: const MeetingsPageContent(),
+      ),
+    );
+  }
+}
+
+class MeetingsPageContent extends StatelessWidget {
+  const MeetingsPageContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(246, 25, 18, 58),
+              Color.fromARGB(255, 71, 58, 100)
+            ],
+            stops: [0.0, 1.0],
+          ),
+        ),
+        child: Column(
+          children: [
+            WindowTitleBarBox(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: MoveWindow(),
+                  ),
+                  const WindowButtons(),
+                ],
+              ),
+            ),
+            const Text(
+              'Reuniões',
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(235, 255, 255, 255),
+                fontSize: 30,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const ResponsibleManager(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_application/lib/pages/navigation.dart
@@ -0,0 +1,45 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_application/pages/registration_page.dart';
+
+import 'meetings_page.dart';
+
+class Navigation extends StatefulWidget {
+  const Navigation({Key? key}) : super(key: key);
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int _currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationView(
+      pane: NavigationPane(
+        selected: _currentPage,
+        onChanged: (i) => setState(() => _currentPage = i),
+        displayMode: PaneDisplayMode.compact,
+        items: <NavigationPaneItem>[
+          PaneItem(
+            icon: const Icon(FluentIcons.calendar_day),
+            title: const Text("Reuniões"),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.contact_card_settings),
+            title: const Text("Cadastros"),
+          ),
+        ],
+      ),
+      content: NavigationBody(
+        index: _currentPage,
+        transitionBuilder: (child, animation) =>
+            EntrancePageTransition(child: child, animation: animation),
+        children: const <Widget>[
+          MeetingsPage(),
+          EditRegistrationPage(),
+        ],
+      ),
+    );
+  }
+}
